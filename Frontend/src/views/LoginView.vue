8875e28c94d7120befe2f31b64eebf57c5d7ae0a
CarlosVeRamz <83312809+Cveramz@users.noreply.github.com>
2023-06-28 02:42:07 -0400

    Update: Cambio en inicio Se abre directamente el login ahora

--- Frontend/src/views/LoginView.vue
+++ Frontend/src/views/LoginView.vue
@@ -13,6 +13,8 @@ export default {
       if (this.validarCorreo()) {
         // Lógica de inicio de sesión aquí
         alert('Formulario enviado y correo válido. Procesando inicio de sesión...');
+        //mostrar en pantalla lo enviado
+        alert('Correo: ' + this.email + '\nContraseña: ' + this.password);
         this.$router.push('#');  // Redirigir a la vista de inicio
       } else {
       }
@@ -31,26 +33,34 @@ export default {
 
 
 <template>
-    <div class="fondo" style="background: #00a499;">
-      <div class="logo" style="background: url(&quot;assets/Logos/Usach%20P2.png&quot;) center / countain no-repeat;"></div>
-      <div class="login">
-        <div class="login__container">
-          <img class="login__logo" src="../assets/Logos/Usach P1.png" alt="Logo">
-          <h4 class="login__title">Bienvenido al Sistema de Tickets</h4>
-          <form class="login__form" @submit.prevent="submitForm">
-            <div class="login">
-              <input class="login__input" type="email" placeholder="Correo Institucional" v-model="email" required>
-            </div>
-            <div class="login">
-              <input class="login__input" type="password" placeholder="Contraseña" v-model="password" required>
-            </div>
-            <button class="login__button" type="submit">Entrar</button>
-          </form>
-          <router-link class="login__link" to="/register">Crear cuenta</router-link>
-          <router-link class="login__link" to="/forgot-password">¿Olvidó su contraseña?</router-link>
-        </div>
+  <div id="app">
+    <div class="logo" style="background: url(&quot;assets/Logos/Usach%20P2.png&quot;) center / contain no-repeat;"></div>
+    <div class="login">
+      <div class="login__container">
+        <img class="login__logo" src="../assets/Logos/Usach P1.png" alt="Logo">
+        <h4 class="login__title">Bienvenido al Sistema de Tickets</h4>
+        <form class="login__form" @submit.prevent="submitForm">
+          <div class="login">
+            <input class="login__input" type="email" placeholder="Correo Institucional" v-model="email" required>
+          </div>
+          <div class="login">
+            <input class="login__input" type="password" placeholder="Contraseña" v-model="password" required>
+          </div>
+          <button class="login__button" type="submit">Entrar</button>
+        </form>
+        
+        <router-link class="login__link" to="/register">Crear cuenta</router-link>
+        <br/>
+        <router-link class="login__link" to="/forgot-password">¿Olvidó su contraseña?</router-link>
       </div>
     </div>
-  </template>
+  </div>
+</template>
 
-<style scoped> /* CSS */</style>
+<style>
+body {
+  background: #00a499;
+  margin: 0;
+  padding: 0;
+}
+</style>
