b12abedfef92081313c772643ef5d9dae818ed1b
CarlosVeRamz <83312809+Cveramz@users.noreply.github.com>
2023-07-23 01:06:47 -0400

    Edit: cambios en el 404

--- Frontend/src/views/NotFoundView.vue
+++ Frontend/src/views/NotFoundView.vue
@@ -1,4 +1,18 @@
 <script>
+export default {
+  mounted() {
+    // Utilizamos setTimeout para redireccionar después de 5 segundos
+    setTimeout(() => {
+      this.redirectToHome();
+    }, 10000);
+  },
+  methods: {
+    redirectToHome() {
+      // Redireccionar a la página de inicio (ruta /)
+      this.$router.push('/');
+    },
+  },
+};
 </script>
 
 
@@ -12,8 +26,9 @@
       </v-col>
 
       <v-card-text class="zona-texto-not-found">
-        <h4 class="register_title">ERROR 404: La página solicitada no se ha podido encontrar.</h4>
-
+        <h4 class="register_title">ERROR 404: La página solicitada no se ha podido encontrar.
+          Serás redireccionado a la página inicial.
+        </h4>
       </v-card-text>
 
     </div>
@@ -28,6 +43,7 @@
   width: 10%;
   font-size: 20px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 }
